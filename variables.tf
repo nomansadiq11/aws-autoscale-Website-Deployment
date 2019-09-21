5ca06c30b7d24d6351b0b0e1e666e52355e7260c
nomansadiq11 <nomi.cs786@gmail.com>
2019-09-21 23:03:51 +0400

    create ec2 instance

--- variables.tf
+++ variables.tf
@@ -1,8 +1,20 @@
 
 
 
-variable "aws_launch_configuration_name" {
-  type        = "string"
-  description = "lanuch configaration "
-  default  = "hw_lc"
+
+variable "ami_name" {
+  type ="string"
+  description = "string"
+  default = "devopstesting"
+}
+variable "ami_id" {
+  type ="string"
+  description = "string"
+  default = "ami-04681a1dbd79675a5"
+
+}
+variable "ami_key_pair_name" {
+  type ="string"
+  description = "string"
+  default = "devopstest"
 }
