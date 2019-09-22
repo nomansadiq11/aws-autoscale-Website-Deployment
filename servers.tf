fd042868bec921b8690de56da0ce0abad7072fc8
nomansadiq11 <nomi.cs786@gmail.com>
2019-09-22 23:11:56 +0400

    update codedepo9y agent and aws role

--- servers.tf
+++ servers.tf
@@ -1,12 +1,16 @@
 
 
 
+
+
 //servers.tf
 resource "aws_instance" "test-ec2-instance" {
-  ami = "${var.ami_id}"
-  instance_type = "t2.micro"
-  key_name = "${var.ami_key_pair_name}"
-  security_groups = ["${aws_security_group.ingress-all-test.id}"]
-
+    
+    ami = "${var.ami_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.ami_key_pair_name}"
+    security_groups = ["${aws_security_group.ingress-all-test.id}"]
     subnet_id = "${aws_subnet.subnet-uno.id}"
+    iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+    
 }
