c2d96d5b98c52e393e72046175d61f24c2a395db
Noman Sadiq <nomi.cs786@gmail.com>
2019-10-07 19:13:15 +0400

    update amazon ami id and run test for create min two instacnes

--- main.tf
+++ main.tf
@@ -1,24 +1,26 @@
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
+
+# ${data.aws_ami.ubuntu.id}
 
 resource "aws_launch_configuration" "as_conf" {
   name_prefix   = "pizzaweb-lc"
-  image_id      = "${data.aws_ami.ubuntu.id}"
+  image_id      = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 
   lifecycle {
@@ -29,7 +31,7 @@ resource "aws_launch_configuration" "as_conf" {
 resource "aws_autoscaling_group" "bar" {
   name                 = "pizzaweb-ag"
   launch_configuration = "${aws_launch_configuration.as_conf.name}"
-  min_size             = 1
+  min_size             = 2
   max_size             = 2
   vpc_zone_identifier       = ["subnet-001852ce35980683c", "subnet-0e9a8efcdbdfe404a"]
 
